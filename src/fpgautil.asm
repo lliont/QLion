3f5fa71446752792664da7d2a0ddc282f8ea69a9
Theodoulos Liontakis <35223175+lliont@users.noreply.github.com>
2026-04-06 03:23:42 +0300

    Add files via upload

--- src/fpgautil.asm
+++ src/fpgautil.asm
@@ -1,4 +1,4 @@
-* Liontakis Theodoulos 2022
+* Liontakis Theodoulos 2024
 
     section fpgautil
 	
@@ -7,11 +7,36 @@
     include dev8_keys_qlv
     include dev8_keys_err
 
+	xref ftable
 	
 cmbit0 equ	$0055
 cmbit1 equ	$00AA
 cmbit3 equ	$5500
 cmbit2 equ	$AA00
+
+;qln_i2c_data    equ $3FF03C
+;qln_i2c_rw_en   equ $3FF03D
+;qln_scr_param   equ 4190208
+
+qln_scr_param   equ $800012
+qln_i2c_data    equ $800021
+qln_i2c_rw_en   equ $800023
+qln_slow        equ $800013
+qln_slowstep    equ $800016
+qln_aud1_fdiv   equ $800050
+qln_aud1_vol    equ $800052
+qln_aud1_har    equ $800053
+qln_aud1_ctrst  equ $800054   
+qln_aud2_fdiv   equ $800058
+qln_aud2_vol    equ $80005A
+qln_aud2_har    equ $80005B
+qln_aud2_ctrst  equ $80005C   
+qln_timer       equ $800030
+qln_timer_div   equ $80003C
+ay_data  		equ $800028 ;8388648
+ay_ctrl  		equ $80002A ;8388650
+
+
 	
 start
         move.w  $110,a2
@@ -20,9 +45,7 @@ start
         moveq   #0,d0
         rts
 		
-pdef    dc.w    12
-;        dc.w    colsb-*
-;        dc.b    6,'COLSUB'
+pdef    dc.w    16
         dc.w    plot-*
         dc.b    5,'FPLOT'
         dc.w    clrsp-*
@@ -37,23 +60,288 @@ pdef    dc.w    12
 		dc.b    4,'FCON'
 		dc.w    fcoff-*
 		dc.b    5,'FCOFF'
-		dc.w    fsound-*
-		dc.b    6,'FSOUND'
+		dc.w    pch256-*
+		dc.b    6,'PCH256'
+		dc.w    cls256-*
+		dc.b    6,'CLS256'
 		dc.w    fsettim-*
 		dc.b    7,'FSETTIM'
-		dc.w    palet8-*
-		dc.b    6,'FPALET'
 		dc.w    ink16-*
 		dc.b    4,'FINK'
 		dc.w    fpaper-*
 		dc.b    6,'FPAPER'
+		dc.w    rtcset-*
+		dc.b    6,'RTCSET'
+		dc.w    ayset-*
+		dc.b    5,'AYSET'
+		dc.w    sound-*
+		dc.b    5,'SOUND'
+		dc.w    fbaud-*
+		dc.b    5,'FBAUD'
         dc.w    0
-        dc.w    2
+        dc.w    3
 		dc.w    ftimer-*
 		dc.b    6,'FTIMER'
-		dc.w    fplay-*
-		dc.b    5,'FPLAY'
+		dc.w    rtcget-*
+		dc.b    6,'RTCGET'
+		dc.w    ayget-*
+		dc.b    5,'AYGET'
         dc.w    0
+		
+ayclk   equ 312500/2   ; 5000000/16 
+
+fbaud
+	move.w  $112,a2
+	jsr     (a2)		
+	subi.w  #1,d3
+	bgt     err1(pc)
+	move.w  0(a6,a1.l),d1  ; colour
+	and.l   #255,d1
+	move.w  d1,$80002C
+	moveq	#0,d0
+	rts
+
+cls256
+	move.w  $112,a2
+	jsr     (a2)		
+	subi.w  #1,d3
+	bgt     err1(pc)
+	move.w  0(a6,a1.l),d1  ; colour
+	and.l   #255,d1
+	move.l  #$4C0000,a2
+	move.l  #$07FFE,d2
+	move.b  d1,d3
+	lsl.w   #8,d1
+	move.b  d3,d1
+clsl1
+	move.w  d1,(a2)
+	addq.l	#2,a2
+	dbra    d2,clsl1
+	moveq	#0,d0
+	rts
+	
+	
+pch256
+	move.w  $112,a2
+	jsr     (a2)
+	subi.w  #4,d3
+	bne     err1(pc)
+	move.w  0(a6,a1.l),d1  ; x
+	and.l   #255,d1
+	move.w  2(a6,a1.l),d0  ; y
+	and.l   #255,d0
+	move.w  4(a6,a1.l),d2  ; color
+	and.l   #255,d2
+	move.w  6(a6,a1.l),d3  ; ch
+	and.l   #255,d3
+	move.l  #$4CFFFF,a2
+	lsl.l   #8,d0
+	sub.l   d0,a2
+	add.l   d1,a2
+	sub.l   #31,d3
+	mulu    #9,d3
+	add     #9,d3
+	lea     ftable,a3
+	add.l   d3,a3
+	moveq.l	#8,d4
+pchl1
+	moveq.l   #5,d0
+	move.b  (a3),d5
+pchl4
+	btst    #7,d5
+	beq     pchl2
+	move.b  d2,(a2)
+pchl2
+	addq.l	#1,a2
+	lsl.l   #1,d5
+	dbra    d0,pchl4
+	add.l	#250,a2
+	addq.l	#1,a3
+	dbra	d4,pchl1	
+	moveq	#0,d0
+	rts
+		
+sound   move.w	sb.gtint,a2
+		jsr	(a2)
+		or.w    d3,d3
+		beq     nosnd
+		cmp.w   #1,d3
+		bne     ayl1
+		move.w  0(a6,a1.l),d3  ; channel
+		move.b  #7,ay_data
+		move.b  #5,ay_ctrl
+		move.b  #5,ay_ctrl
+		move.b  #0,ay_ctrl
+		move.b  #1,ay_ctrl
+		move.b  #1,ay_ctrl
+		move.b  #0,ay_ctrl
+		moveq   #0,d1
+		move.b  ay_data,d2
+		bset    d3,d2 
+		move.b  #7,d1
+		jmp     aset  
+ayl1	subq.w	#3,d3
+		bne   	err1(pc)
+		move.w  0(a6,a1.l),d3  ; channel
+		move.w  2(a6,a1.l),d4  ; freq
+		move.w  4(a6,a1.l),d5  ; volume
+		move.l  #ayclk,d6
+		and.l   #$0000FFFF,d4
+		divu    d4,d6   ; step count
+		and.l   #$0000FFFF,d6
+        move.w  d3,d1
+		add.w   #8,d1   ; volume register
+		move.b  d5,d2
+		bsr     aset
+		move.w  d3,d1
+		lsl.w   #1,d1
+		move.b  d6,d2 
+		bsr     aset 
+		lsr.w   #8,d6
+		and.l   #15,d6
+		addq    #1,d1
+		move.b  d6,d2 
+		bsr     aset
+		move.b  #7,ay_data
+		move.b  #5,ay_ctrl
+		move.b  #5,ay_ctrl
+		move.b  #0,ay_ctrl
+		move.b  #1,ay_ctrl
+		move.b  #1,ay_ctrl
+		move.b  #0,ay_ctrl
+		moveq   #0,d1
+		move.b  ay_data,d2   ; get reg 7 value
+		bclr    d3,d2 
+		moveq   #7,d1
+		bsr     aset		
+		moveq	#0,d0
+		rts
+		
+nosound move.w	sb.gtint,a2
+		jsr	(a2)
+		or.w	d3,d3
+        bne   	err1(pc)
+nosnd   move.b  #255,d2
+		move.b  #7,d1
+		jmp     aset 
+		
+ayget   move.w	sb.gtint,a2
+		jsr	(a2)
+		subq.w	#1,d3
+		bne   	err1(pc)
+		move.w  0(a6,a1.l),d1
+		move.l  $58(a6),a1
+		subq.l	#2,a1
+		move.l  a1,$58(a6)
+
+        move.b  d1,ay_data
+		move.b  #5,ay_ctrl
+		move.b  #5,ay_ctrl
+		move.b  #0,ay_ctrl
+		move.b  #1,ay_ctrl
+		move.b  #1,ay_ctrl
+		move.b  #0,ay_ctrl
+		moveq   #0,d1
+		move.b  ay_data,d1
+		
+		move.w  d1,(a6,a1.l)
+		moveq   #3,d4
+		moveq	#0,d0
+		rts		
+
+ayset   move.w	sb.gtint,a2
+		jsr	(a2)
+		subq.w	#2,d3
+		bne   	err1(pc)
+		move.w  0(a6,a1.l),d1
+		move.w  2(a6,a1.l),d2
+aset    move.b  d1,ay_data
+		move.b  #5,ay_ctrl
+		move.b  #5,ay_ctrl
+		move.b  #0,ay_ctrl
+		move.b  d2,ay_data
+		move.b  #4,ay_ctrl
+		move.b  #4,ay_ctrl
+		move.b  #0,ay_ctrl
+		moveq	#0,d0
+		rts
+		
+rtcset
+	move.w	sb.gtint,a2
+	jsr	(a2)
+	subq.w	#2,d3
+	bne   	err1(pc)
+	move.w  0(a6,a1.l),d0
+	move.w  2(a6,a1.l),d5
+	move.l  $58(a6),a1
+	subq.l	#2,a1
+	move.l  a1,$58(a6)
+	move.l   #qln_i2c_data,a2
+	move.l   #qln_i2c_rw_en,a3
+	move.b  d0,(a2)
+	move.b  #1,(a3) ;write
+rtcs3 move.b   (a3),d2
+	btst    #0,d2
+    beq.s   rtcs3
+;	move.b  #2,(a3) ; stop
+	move.b  d5,(a2)
+rtcs move.b   (a3),d2
+	btst    #0,d2
+    bne.s   rtcs
+	move.b  #1,(a3) ;write 
+rtcs4 move.b   (a3),d2
+	btst    #0,d2
+    beq.s   rtcs4
+	move.b  #2,(a3) ; stop
+rtcs2 move.b   (a3),d2
+	btst    #0,d2
+    bne.s   rtcs2
+	moveq	#0,d0
+	rts
+
+
+rtcget
+	move.w	sb.gtint,a2
+	jsr	(a2)
+	subq.w	#1,d3
+	bne   	err1(pc)
+	move.w  0(a6,a1.l),d0
+	move.l  $58(a6),a1
+	subq.l	#2,a1
+	move.l  a1,$58(a6)
+	move.l   #qln_i2c_data,a2
+	move.l   #qln_i2c_rw_en,a3
+	move.b  d0,(a2)
+	move.b  #1,(a3) ;write
+rtcw3 move.b   (a3),d2
+	btst    #0,d2
+    beq.s   rtcw3    ; wait to start transmission
+	move.b  #2,(a3) ; stop
+rtcw move.b   (a3),d2
+	btst    #0,d2
+    bne.s   rtcw    ; wait to end;
+	move.b  #3,(a3) ;read
+rtcw4 move.b   (a3),d2
+	btst    #0,d2
+    beq.s   rtcw4   ; wait to start transmission
+	move.b  #2,(a3) ; stop
+rtcw2 move.b   (a3),d2
+	btst    #0,d2
+    bne.s   rtcw2
+	moveq   #0,d1
+	move.b  (a2),d1
+	cmp.b   #7,d0
+	bgt.s   skip_bcd
+	move.l  d1,d2
+	lsr.b   #4,d2
+	mulu    #10,d2
+	and.b   #$0F,d1
+	add.b   d2,d1 
+skip_bcd
+	move.w  d1,(a6,a1.l)
+	moveq   #3,d4
+	moveq	#0,d0
+	rts
 
 bi_gtchn
          moveq    #1,d7             default channel no.
@@ -197,97 +485,70 @@ palet8
         bne     err1(pc)
 		move.w  0(a6,a1.l),d0  ; p
         and.b   #7,d0
-		move.b  (4190208),d1
+		move.b  (qln_scr_param),d1
 		and.b    #$F1,d1
 		lsl.b	#1,d0
 		or.b     d0,d1 
-		move.b   d1,(4190208)
+		move.b   d1,(qln_scr_param)
 		bra      exit(pc)
-fplay   move.l  $58(a6),a1
-		subq.l	#2,a1
-		move.l  a1,$58(a6)
-		moveq   #0,d1
-		move.b  4190228,d1
-		move.w  d1,(a6,a1.l)
-		moveq   #3,d4
-		moveq	#0,d0
-        rts
+		
 fsettim move.w  $112,a2
         jsr     (a2)
         subi.w  #1,d3
         bne     err1(pc)
 		move.w  0(a6,a1.l),d0
-		move.w  d0,4190212
+		move.w  d0,(qln_timer_div)
 		moveq	#0,d0
         rts
 ftimer  move.l  $58(a6),a1
 		subq.l	#2,a1
 		move.l  a1,$58(a6)
-		move.w  4190222,d1
+		move.l  (qln_timer),d1
 		move.w  d1,(a6,a1.l)
 		moveq   #3,d4
 		moveq	#0,d0
         rts
-fsound
-		move.w  $112,a2
-        jsr     (a2)
-        subi.w  #4,d3
-        bne     err1(pc)
-		move.w  0(a6,a1.l),d0  ; freq
-        and.l   #$FFF,d0
-        move.w  2(a6,a1.l),d1  ; duration
-        and.l   #$F,d1
-		move.w  4(a6,a1.l),d2  ; volume
-        and.l   #$FF,d2
-		move.w  6(a6,a1.l),d3  ; harmonic
-        and.l   #7,d3
-		move.l  #100000,d4		
-		divu    d0,d4          ; fbase/f
-		and.l   #$0000FFFF,d4
-		sub.w   #1,d4
-		lsl.w   #8,d1
-		lsl.w   #5,d1
-		or.w    d1,d4
-		move.w  d4,(4190224)
-		move.b  d2,(4190226)
-		move.b  d3,(4190227)
-		move.b  #0,(4190228)
-		move.b  #1,(4190228)
-		moveq   #0,d0
-        rts
+		
 fcon 
-		move.b  (4190208),d1
+		move.b  (qln_scr_param),d1
 		or.b     #1,d1 
-		move.b   d1,(4190208)
+		move.b   d1,(qln_scr_param)
 		bra      exit(pc) 
 fcoff 
-		move.b  (4190208),d1
+		move.b  (qln_scr_param),d1
 		and.b    #$FE,d1 
-		move.b   d1,(4190208)
+		move.b   d1,(qln_scr_param)
 		bra      exit(pc)
-fslow 
-		move.b  (4190209),d1
+fslow 	move.w   $112,a2
+        jsr     (a2)
+		cmp.w	#1,d3
+		bgt     err1(pc)
+        subi.w  #1,d3
+		bne     fslow1
+		move.w  0(a6,a1.l),d1
+		move.w  d1,(qln_slowstep)
+fslow1	move.b  (qln_slow),d1
 		or.b     #1,d1 
-		move.b   d1,(4190209)
+		move.b   d1,(qln_slow)
 		bra      exit(pc) 
 ffast  
-		move.b  (4190209),d1
+		move.b  (qln_slow),d1
 		and.b    #$FE,d1 
-		move.b   d1,(4190209)
+		move.b   d1,(qln_slow)
 		bra      exit(pc)
 setsp   move.w  $112,a2
         jsr     (a2)
         subi.w  #4,d3
         bne     err1(pc)
 		move.w  0(a6,a1.l),d0  ; s
-        and.l   #63,d0
+        and.l   #127,d0
         move.w  2(a6,a1.l),d1  ; en
         and.l   #1,d1
         move.w  4(a6,a1.l),d2  ; x
         and.l   #$FF,d2
         move.w  6(a6,a1.l),d3  ; y
 		and.l   #$FF,d3
-		move.l  #$3fc000,a2
+		move.l  #$C00000,a2
 		move.l  d0,d4
 		divu    #30,d4   ; bank
 		move.l  d4,d5
@@ -301,11 +562,11 @@ setsp   move.w  $112,a2
 		move.b  d2,1(a2)
 		move.b  d3,2(a2)
 		bra     exit(pc)
-clrsp   move.l  #$3fc000,a1
+clrsp   move.l  #$C00000,a1
         move.l  #63,d3
 lp2     move.l  #0,(a1)+
         dbra    d3,lp2(pc) 
-		move.l  #$3fc000+4096,a1
+		move.l  #$C00000+4096,a1
         move.l  #63,d3
 		bra.s   lp1
 cls64   move.l  #$1C000,a1
