f6e5ec53cf4fb7c1d6b117b93fab345c8ef56cec
Theodoulos Liontakis <35223175+lliont@users.noreply.github.com>
2026-04-12 16:31:25 +0300

    Add files via upload

--- src/ymplay.c
+++ src/ymplay.c
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------*/
-/*     Liontakis Theodoulos 2005  Simple .ym audio file player           */
+/*     SD card fat directory     Liontakis Theodoulos                    */
 /*-----------------------------------------------------------------------*/
 #include "stdio.h"
 #include <qdos.h>
@@ -24,12 +24,12 @@ int (*_Open)(const char *, int, ...) = qopen;
 char *_endmsg = NULL;
 void (*consetup)() = NULL;
 
-void sdelay()
+void sdelay(unsigned char dl)
 {
 unsigned long t1,t2;
 t1=ReadLong(0x800030);
 t2=t1;
-do { t2=ReadLong(0x800030); } while (abs(t2-t1)<19L);
+do { t2=ReadLong(0x800030); } while (abs(t2-t1)<dl);
 return;
 }
 
@@ -48,14 +48,14 @@ void aywrite(unsigned char r, unsigned char c)
 
 void main(int argc, char *argv[])
 {
-	int ff, ft,mono=0;
+	int ff, ft, mono=0;
 	int key;
-	char fname1[80],header[34],ddl[4];
-    char *buffer, c, j;
+	unsigned char fname1[80],header[34],ddl[4];
+    unsigned char *buffer, c, j;
 	unsigned char attribute , v;
-	unsigned long frames, ddln, freq, f, ratio, vl,v1,v2;
+	unsigned long frames, ddln, freq, f, ratio, vw,v1,v2;
 	unsigned int  br, i, k, rd1,rd2;        
-	unsigned short digidrums, sp, d;
+	unsigned short digidrums, sp, d, rate;
 	WriteWord(0x80003C,10000);          /* set timer to 1 msec */
 	printf("Playing %s\n",argv[1]);
 	if (argc<2) { printf(" Usage: ew ymplay[,#channel];'filename [chip frequency(Khz)]'\n"); return; }
@@ -69,7 +69,10 @@ void main(int argc, char *argv[])
 	frames=ReadLong(&header[12]);
 	digidrums= ReadWord(&header[20]);
 	freq= ReadLong(&header[22])/1000L;
+	rate= ReadWord(&header[26]);
 	printf("Original Frequency %d Khz\n",freq);
+	printf("Rate %d hz\n",rate);
+	rate=1000L/rate;
 	attribute=ReadByte(&header[19]);
 	if (digidrums>0)  {
 		for (i=0;i<digidrums;i++) {
@@ -79,8 +82,10 @@ void main(int argc, char *argv[])
 	}
 	printf("%d frames, %d digidrums \n",frames,digidrums);
 	do {read(ft,&c,1); printf("%c",c); } while (c!=0);
-	do {read(ft,&c,1);} while (c!=0); 
-	do {read(ft,&c,1);} while (c!=0);
+	printf("\n");
+	do {read(ft,&c,1);  printf("%c",c);} while (c!=0); 
+	printf("\n");
+	do {read(ft,&c,1);  printf("%c",c);} while (c!=0);
 	printf("\n");
 	buffer=(char *) malloc(frames*16);
 	rd1=read(ft, buffer, frames*16);
@@ -92,45 +97,57 @@ void main(int argc, char *argv[])
 			for (j=0;j<14;j++) 
 			{	
 				if (j<6)  { 
-					if (j % 2 == 1) {
+					if (j%2 == 1) {
 						v1=(unsigned long) buffer[i+(j-1)*frames];
 						v2=(unsigned long) buffer[i+j*frames];
-						vl = ((v1 | (v2 << 8))* f ) / freq;
-						v = (unsigned char) (vl & 0x00FF);
+						vw = ((v1 | ((v2 & 0xF) << 8))* f ) / freq;
+						v = (unsigned char) (vw & 0x00FF);
 						aywrite(j-1,v);
-						v = (unsigned char) ((vl >> 8) & 0x00F);
+						v = (unsigned char) ((vw >> 8) & 0x000F);
 						aywrite(j,v);
 					}
 				}
 				else if (j == 6)
 				{
-						v1=(unsigned long) (0x1F & buffer[i+6*frames]);
-						vl = (v1 * f ) / freq;
-						if (vl>31) v=31; else v = (unsigned char) (vl & 0x01F);
-						aywrite(6,v);
+						v1=(unsigned long) (0x1F & buffer[i+j*frames]);
+						vw = (v1 * f ) / freq;
+						if (vw>31) v=31; else v = (unsigned char) (vw & 0x01F);
+						aywrite(j,v);
+				}
+				else if (j == 7)
+				{
+					aywrite(j,0x3F & buffer[i+j*frames]);
+				}
+				else if (j < 11)
+				{
+					aywrite(j,0x1F & buffer[i+j*frames]);
 				}
 				else if (j == 11 || j == 12)
 				{
 					if (j==12) {
 						v1=(unsigned long) buffer[i+11*frames];
 						v2=(unsigned long) buffer[i+12*frames];
-						vl = ((v2 | (v1 << 8))* f ) / freq;
-						if (v1>0xFFFF) {
+						vw = ((v1 | (v2 << 8))* f ) / freq;
+						if (vw>0xFFFF) {
 							aywrite(11,255);
 							aywrite(12,255);
 						} else {
-							v = (unsigned char) (vl & 0x00FF);
+							v = (unsigned char) (vw & 0x00FF);
 							aywrite(11,v);
-							v = (unsigned char) ((vl >> 8) & 0x00FF);
+							v = (unsigned char) ((vw >> 8) & 0x00FF);
 							aywrite(12,v);
 						}
 					}
 				}
+				else if (j == 13)
+				{
+					aywrite(j,0x0F & buffer[i+j*frames]);
+				}
 				else aywrite(j,buffer[i+j*frames]);
 			}
 			/* aywrite(7,buffer[i+7*frames]); */
-			sdelay();
-						key=keyrow(1);
+			sdelay(rate);
+			key=keyrow(1);
 			if (key==8) i=frames;
 		}
 	} else {
@@ -143,41 +160,53 @@ void main(int argc, char *argv[])
 					if (j % 2 == 1) {
 						v1=(unsigned long) buffer[i*16+j-1];
 						v2=(unsigned long) buffer[i*16+j];
-						vl = ((v1 | (v2 << 8))* f ) / freq;
-						v = (unsigned char) (vl & 0x00FF);
+						vw = ((v1 | ((v2 & 0xF) << 8))* f ) / freq;
+						v = (unsigned char) (vw & 0x00FF);
 						aywrite(j-1,v);
-						v = (unsigned char) (vl >> 8) & 0x00F;
+						v = (unsigned char) (vw >> 8) & 0x000F;
 						aywrite(j,v);
 					}
 				}
 				else if (j == 6)
 				{
 						v1=(unsigned long)(0x1F & buffer[i*16+6]);
-						vl = (v1 * f ) / freq;
-						if (vl>31) v=31; else v = (unsigned char) (vl & 0x001F);
+						vw = (v1 * f ) / freq;
+						if (vw>31) v=31; else v = (unsigned char) (vw & 0x001F);
 						aywrite(6,v);
+				}
+				else if (j == 7)
+				{
+					aywrite(j,0x3F & buffer[i*16+j]);
+				}
+				else if (j < 11)
+				{
+					aywrite(j,0x1F & buffer[i*16+j]);
 				}
 				else if (j == 11 || j == 12)
 				{
 					if (j==12) {
 						v1=(unsigned long) buffer[i*16+11];
 						v2=(unsigned long) buffer[i*16+12];
-						vl = ((v2 | (v1 << 8))* f ) / freq;
-						if (v1>0xFFFF) {
+						vw = ((v1 | (v2 << 8))* f ) / freq;
+						if (vw>0xFFFF) {
 							aywrite(11,255);
 							aywrite(12,255);
 						} else {
-							v = (unsigned char) (vl & 0x00FF);
+							v = (unsigned char) (vw & 0x00FF);
 							aywrite(11,v);
-							v = (unsigned char) ((vl >> 8) & 0x0FF);
+							v = (unsigned char) ((vw >> 8) & 0x0FFL);
 							aywrite(12,v);
 						}
 					}
 				}
+				else if (j == 13)
+				{
+					aywrite(j,0x0F & buffer[i*16+j]); 
+				}
 				else aywrite(j,buffer[i*16+j]);
 			}
 			/* aywrite(7,buffer[i*16+7]); */
-			sdelay();
+			sdelay(rate);
 			key=keyrow(1);
 			if (key==8) i=frames;
 		}
